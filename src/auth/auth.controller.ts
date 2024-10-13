3d43200c37fea5c6a9d7d621a8153344b1099cf5
Bishoy Mikhael <bishoy.mikhael@dar.com>
2024-10-13 12:05:56 +0300

    Setup AuthModule structure in controller and service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dtos/create-user.dto';
+import { LoginUserDto } from './dtos/login-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService
+  ) { }
+
+  @Post('/register')
+  registerUser(@Body() createUserDto: CreateUserDto) {
+    return this.authService.registerUser(createUserDto);
+  }
+
+  @Post('/login')
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.loginUser(loginUserDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+import { HashingProvider } from './providers/hashing.provider';
+import { LoginUserDto } from './dtos/login-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UsersService,
+    private readonly hashingProvider: HashingProvider
+  ) { }
+
+  async registerUser(createUserDto: CreateUserDto) {
+    // 1. check if user already exists
+    // 2. create user
+    // 3. hash password
+    // 4. store password
+    // 5. generate JWT
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    // 1. check if user already exists
+    // 2. load user
+    // 3. hash input password
+    // 4. generate JWT
+  }
+}
