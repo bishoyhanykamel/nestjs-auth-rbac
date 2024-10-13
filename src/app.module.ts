207dae723436357415b609e677430374c467fef5
Bishoy Mikhael <bishoy.mikhael@dar.com>
2024-10-13 07:41:16 +0300

    Add skeleton reviews module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { BooksModule } from './books/books.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [AuthModule, BooksModule],
+  imports: [AuthModule, BooksModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
