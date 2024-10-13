0d0ce7ab5ec21c7daf9e67adf5ec4db9b803906f
Bishoy Mikhael <bishoy.mikhael@dar.com>
2024-10-13 11:41:43 +0300

    implement user controller and service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, DefaultValuePipe, Get, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    private readonly usersService: UsersService
+  ) { }
+
+  @Get()
+  getUsers(@Query('email', new DefaultValuePipe('')) email: string) {
+    if (email.length == 0)
+      return this.usersService.getAll();
+
+    return this.usersService.getUserByEmail(email);
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
+
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Like, Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>
+  ) { }
+
+  async getAll() {
+    return this.userRepo.find();
+  }
+
+  async getUserByEmail(email: string) {
+    return this.userRepo.find({
+      where: {
+        email: Like(`%${email}%`)
+      }
+    })
+  }
+
+  async createUser(createUserDto: CreateUserDto) {
+    let user = this.userRepo.create(createUserDto);
+
+    return await this.userRepo.save(user);
+  }
+}
