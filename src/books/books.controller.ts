218e3c764287c5708e83499cb2fd441e90382a12
Bishoy Mikhael <bishoy.mikhael@dar.com>
2024-10-13 10:39:21 +0300

    implement books controller and service

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { CreateBookDto } from './dtos/create-book.dto';
+import { UpdateBookDto } from './dtos/update-book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(
+    private readonly booksService: BooksService
+  ) { }
+
+  @Get()
+  getAllBooks() {
+    return this.booksService.getAllBooks();
+  }
+
+  @Get('/:id')
+  getBookById(@Param('id', ParseIntPipe) id: number) {
+    return this.booksService.getBookById(id);
+  }
+
+  @Post()
+  createBook(@Body() createBookDto: CreateBookDto) {
+    return this.booksService.createBook(createBookDto);
+  }
+
+  @Patch('/:id')
+  updateBookById(@Param('id', ParseIntPipe) id: number, @Body() updateBookDto: UpdateBookDto) {
+    return this.booksService.updateBookById(id, updateBookDto);
+  }
+
+  @Delete('/:id')
+  deleteBookById(@Param('id', ParseIntPipe) id: number) {
+    return this.booksService.deleteBookById(id);
+  }
+}
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from './book.entity';
+import { Repository } from 'typeorm';
+import { CreateBookDto } from './dtos/create-book.dto';
+import { UpdateBookDto } from './dtos/update-book.dto';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(
+    @InjectRepository(Book)
+    private readonly bookRepo: Repository<Book>
+  ) { }
+
+  async getAllBooks() {
+    return await this.bookRepo.find();
+  }
+
+  async getBookById(id: number) {
+    return await this.bookRepo.find({
+      where: {
+        id
+      }
+    })
+  }
+
+  async createBook(createBookDto: CreateBookDto) {
+    const createdBook = this.bookRepo.create(createBookDto);
+    return await this.bookRepo.save(createdBook);
+  }
+
+  async updateBookById(id: number, updateBookDto: UpdateBookDto) {
+    const existingBook = await this.bookRepo.find({
+      where: {
+        id
+      }
+    });
+
+    if (existingBook.length <= 0)
+      throw new BadRequestException('Book does not exist');
+
+    return await this.bookRepo.update({ id }, { ...existingBook, ...updateBookDto });
+  }
+
+  async deleteBookById(id: number) {
+    const existingBook = await this.bookRepo.find({
+      where: {
+        id
+      }
+    });
+
+    if (existingBook.length <= 0)
+      throw new BadRequestException('Book does not exist');
+
+    return await this.bookRepo.delete(id);
+  }
+}
