2c5bec0294f994460f7bc6d752fc44dd837edaf3
Bishoy Mikhael <bishoy.mikhael@dar.com>
2024-10-13 10:59:09 +0300

    add preventative code for book service

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Book } from './book.entity';
 import { Repository } from 'typeorm';
@@ -13,28 +13,70 @@ export class BooksService {
   ) { }
 
   async getAllBooks() {
-    return await this.bookRepo.find();
+    let books = undefined;
+    try {
+      books = await this.bookRepo.find();
+    } catch (err) {
+      throw new InternalServerErrorException('Connection to database failed.');
+    }
+
+    if (books == undefined)
+      throw new InternalServerErrorException('Could not load books.');
+
+    return books;
   }
 
   async getBookById(id: number) {
-    return await this.bookRepo.find({
-      where: {
-        id
-      }
-    })
+    let book = undefined;
+
+    try {
+      book = await this.bookRepo.find({
+        where: {
+          id
+        }
+      });
+    } catch (err) {
+      throw new InternalServerErrorException('Could not connect to the database.');
+    }
+
+    if (book == undefined)
+      throw new InternalServerErrorException('Could not load book.');
+
+    if (book.length <= 0)
+      throw new NotFoundException(`Book does not exist.`);
+
+    return book[0];
   }
 
   async createBook(createBookDto: CreateBookDto) {
-    const createdBook = this.bookRepo.create(createBookDto);
+    let createdBook = undefined;
+    try {
+      createdBook = this.bookRepo.create(createBookDto);
+    }
+    catch (err) {
+      throw new InternalServerErrorException('Could not communicate with database');
+    }
+    if (createdBook == undefined)
+      throw new InternalServerErrorException('Could not create book.');
+
     return await this.bookRepo.save(createdBook);
   }
 
   async updateBookById(id: number, updateBookDto: UpdateBookDto) {
-    const existingBook = await this.bookRepo.find({
-      where: {
-        id
-      }
-    });
+    let existingBook = undefined;
+
+    try {
+      existingBook = await this.bookRepo.find({
+        where: {
+          id
+        }
+      });
+    } catch (err) {
+      throw new InternalServerErrorException('Could not connect to the database.');
+    }
+
+    if (existingBook == undefined)
+      throw new InternalServerErrorException('Could not load an existing book.');
 
     if (existingBook.length <= 0)
       throw new BadRequestException('Book does not exist');
@@ -43,11 +85,20 @@ export class BooksService {
   }
 
   async deleteBookById(id: number) {
-    const existingBook = await this.bookRepo.find({
-      where: {
-        id
-      }
-    });
+    let existingBook = undefined;
+
+    try {
+      existingBook = await this.bookRepo.find({
+        where: {
+          id
+        }
+      });
+    } catch (err) {
+      throw new InternalServerErrorException('Could not connect to the database.');
+    }
+
+    if (existingBook == undefined)
+      throw new InternalServerErrorException('Could not load an existing book.');
 
     if (existingBook.length <= 0)
       throw new BadRequestException('Book does not exist');
